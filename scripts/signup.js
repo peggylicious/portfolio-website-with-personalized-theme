388a153f03d9bd4711bbcc1fafd3d8ae7e30cb2d
peggylicious <margaret.ogbor@pether.io>
2022-02-16 22:37:07 +0100

    Add Id to password field and create payload/refactor signup script

--- scripts/signup.js
+++ scripts/signup.js
@@ -11,54 +11,66 @@ let loginBtn = document.querySelector("#login-submit-btn");
 // let formData = new FormData(myform);
 // let data = {}
 
-function getDetails(datas){
-    // loginData = new FormData(myform);
-    authenticate(datas).then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => {
-      console.log(err);
-    });
-}
-let data;
+// function getDetails(datas) {
+//   // loginData = new FormData(myform);
+//   authenticate(datas)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+// let data;
 btn.addEventListener("click", function (event) {
-    let username = document.getElementById("name-1").value;
-let useremail = document.getElementById("email-1").value;
-let formData = new FormData(myform);
-data = {"name": username+"",
-"email": useremail+""}
+  let username = document.getElementById("name-1").value;
+  let useremail = document.getElementById("email-1").value;
+  let formData = new FormData(myform);
+  // data = {
+  //   name: username + "",
+
+  //   email: useremail + "",
+  // };
+
+  let signupData = {
+    name: document.getElementById("name-1").value,
+    email: document.getElementById("email-1").value,
+    password: document.getElementById("password").value,
+    confirmPassword: document.getElementById("confirmPassword").value,
+  };
   event.preventDefault();
-    //   event.stopImmediatePropagation();
+  //   event.stopImmediatePropagation();
 
-    console.log( document.getElementById("email-1").value)
-  console.log(data);
+  // console.log(document.getElementById("email-1").value);
+  // console.log(data);
 
-    authenticate().then((response) => response.json())
+  authenticate(signupData)
+    .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((err) => {
       console.log(err);
     });
 });
 // }
-async function authenticate() {
-    console.log("Data, ", data)
-    let response = await fetch("http://localhost:3000/user/signup", {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(data)
-    });
-    // console.log(JSON.stringify(data))
-    return response
+async function authenticate(data) {
+  // console.log("Data, ", data);
+  let response = await fetch("http://localhost:3000/user/signup", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  // console.log(JSON.stringify(data))
+  return response;
 }
 
-async function authenticate1(datas) {
-    console.log("Data, ", datas)
-    let response = await fetch("http://localhost:3000/user/signup", {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(datas)
-    });
-    // console.log(JSON.stringify(data))
-    return response
-}
+// async function authenticate1(datas) {
+//   console.log("Data, ", datas);
+//   let response = await fetch("http://localhost:3000/user/signup", {
+//     method: "POST",
+//     headers: { "content-type": "application/json" },
+//     body: JSON.stringify(datas),
+//   });
+//   // console.log(JSON.stringify(data))
+//   return response;
+// }
 
 // loginBtn.addEventListener("click", getDetails(loginData))
